2a2daa40786e3612e3433d6be6e1ad56574203b5
Filip Dimitrov <github@filip-dimitrov.eu>
2020-09-04 11:22:29 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ module "submodule" {
 #output "test1" {
 #  value = module.submodule.test1
 #}
+
+resource "null_resource" "always" {
+  triggers = {
+    build_number = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    command = "ls -laR /terraform"
+  }
+}
